8389d4560389cee01c926531caf9b15cce7c7add
william3321 <williamnylander01@gmail.com>
2024-01-02 05:51:43 +0100

    modified:   index.html 	deleted:    main_news.js 	modified:   news.html

--- main_news.js
+++
@@ -1,3 +0,0 @@
-var newsMovieData = movies.find(movie => movie.id === 8); // Assuming you want a different movie
-var newsMovieCard = new MovieCard(newsMovieData);
-newsMovieCard.createCard("movieContainer3", [7, 8, 9, 10, 11, 12]);
